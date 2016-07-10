80b9a5361c2479e59004ce660b2cc2cf27cc8a7d
MadhanB <smartmadhan07@gmail.com>
2016-07-10 12:01:41 +0530

    finish

--- README.c
+++ README.c
@@ -3,16 +3,16 @@
 void main()
 {
 int a,b,c,e=1;
-printf("Enter the n values:");
+printf("enter the n values:");
 scanf("%d",&a);
-printf("Enter the pair:");
+printf("enter the no of pair:");
 scanf("%d",&c);
 for(int d=0;d<2*c;d++)
 {
 b=2*a-d;
 if(c!=b)
 {
-printf("The pair is %d\n",c,e++,c);
+printf("the pair is %d",c,e++,c);
 }
 }
 }
